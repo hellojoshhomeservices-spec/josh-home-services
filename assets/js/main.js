719076ba1950ecd0354e72f7fd6f451d6333e180
hellojoshhomeservices-spec <hello.joshhomeservices@gmail.com>
2026-09-22 12:14:56 +0200

    feat: establish core JavaScript foundation

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Josh Home Services
+ * Main website JavaScript
+ * Version 1.0
+ */
+
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Josh Home Services website loaded.");
+});
